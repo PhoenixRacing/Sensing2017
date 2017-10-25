373a01727a30990f0c2b3ef82974951e7361eb80
AnyaCakes <anyaljensen@gmail.com>
2017-10-24 21:19:24 -0400

    Add tachometer average code

---
+++ Tachometer_Ave10/sketch_oct19a/sketch_oct19a.ino
@@ -0,0 +1,41 @@
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+float total = 0;                  // the running total
+float average = 0;                // the average
+
+int inputPin = A0;
+
+void setup() {
+  // initialize serial communication with computer:
+  Serial.begin(9600);
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+}
+
+void loop() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(inputPin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+
+  // calculate the average:
+  average = total / float(numReadings);
+  // send it to the computer as ASCII digits
+  float y = 5*average / 1024;
+  Serial.println(y);
+  delay(.01);        // delay in between reads for stability
+}
